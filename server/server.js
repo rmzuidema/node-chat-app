e66bcbb088fd630c3b6da0d32a4cb4c72a346f17
rmzuidema <robert.m.zuidema@gmail.com>
2017-12-12 15:21:13 -0500

    Added connect and disconnect messages

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+
+var socketIO = require('socket.io');
+
 const publicPath = path.join(__dirname, '/../public');
 
 //console.log(__dirname+ '/../public');
@@ -7,10 +11,28 @@ const publicPath = path.join(__dirname, '/../public');
 
 var app = express();
 app.use('/', express.static(publicPath));
+// var server = http.createServer((req, res) => {
+
+// });
+
+// can be used as:
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket)=> {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected from client');
+    });
+});
+
+
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+// called with server instead of app
+server.listen(port, () => {
     console.log(`Server started in port ${port}`);
 });
 
